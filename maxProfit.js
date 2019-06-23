b1de5376cfbd8c2ed582765524f5c9ab6c227d91
jinfull <jinfull@gmail.com>
2019-06-23 12:42:55 -0700

    Add optimized solution for maxProfit - single loop with pointer to minPrice

--- maxProfit.js
+++ maxProfit.js
@@ -15,3 +15,19 @@ var maxProfit = function(prices) {
     
     return maxProfit;
 };
+
+// optimized O(n)
+var maxProfit = function(prices) {
+    let minPrice = prices[0];
+    let maxProfit = 0;
+    
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
+        }
+    }
+    
+    return maxProfit;
+};
