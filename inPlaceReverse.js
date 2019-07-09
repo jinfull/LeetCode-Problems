a573ca556754586fcf767ece94de246eb659c02e
jinfull <jinfull@gmail.com>
2019-07-09 14:12:32 -0700

    Add inPlaceReverse of charArr

---
+++ inPlaceReverse.js
@@ -0,0 +1,31 @@
+// function inPlaceReverse(charArr) {
+// 	let j = charArr.length - 1;
+
+// 	for (let i = 0; i < charArr.length; i++) {
+// 		if (i > j) break;
+
+// 		[charArr[i], charArr[j]] = [charArr[j], charArr[i]];
+// 	    j--;
+// 	}
+
+// 	return charArr;
+// }
+
+
+// alternative solution using while loop
+
+function inPlaceReverse(charArr) {
+	let i = 0;
+	let j = charArr.length - 1;
+
+	while (i < j) {
+		[charArr[i], charArr[j]] = [charArr[j], charArr[i]];
+
+		i++;
+		j--;
+	}
+
+  return charArr;
+}
+
+inPlaceReverse(['h','e','l','l','o'])
